9a37844140ffe8d98ee9408ffd9524bdc3a5c718
Ivan Silva <ivankuartz@hotmail.com>
2022-05-10 16:32:23 -0300

    Created decorators and tested them

--- person.rb
+++ person.rb
@@ -1,9 +1,13 @@
-class Person
+require './nameable'
+require './decorator'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = id
+    super()
+    @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -13,9 +17,25 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, name: 'maximilianus')
+
+person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+
+puts capitalized_trimmed_person.correct_name
